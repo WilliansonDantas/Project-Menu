b021bbf52504b7946541dbb782ca9d1a9b5fcdcd
WilliansonDantas <williansondantas@gmail.com>
2023-04-10 20:24:36 -0300

    feat:cria rotas solicitadas de product

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 import CategoryController from '../Controllers/CategoryController';
+import ProductController from '../Controllers/ProductController';
 
 const routes = Router();
 
 routes.get('/category', (req, res, next) => new CategoryController(req, res, next).findAll());
 
+routes.get('/product/:id', (req, res, next) => new ProductController(req, res, next).findById());
+
+routes.get('/product', (req, res, next) => new ProductController(req, res, next).findAll());
+
+routes.patch('/product/:id', (req, res, next) => new ProductController(req, res, next).update());
+
+routes.post('/product', (req, res, next) => new ProductController(req, res, next).create());
+
+routes.delete('/product/:id', (req, res, next) => new ProductController(req, res, next).delete());
 
 export default routes;
