a87977bb5115e0fb0450680802f4839d4a35d24f
WilliansonDantas <williansondantas@gmail.com>
2023-04-10 21:56:46 -0300

    feat:cria rota de autenticação

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -1,19 +1,35 @@
 import { Router } from 'express';
 import CategoryController from '../Controllers/CategoryController';
 import ProductController from '../Controllers/ProductController';
+import AuthController from '../Controllers/AuthController';
+import ValidateJWT from '../Middlewares/ValidateJWT';
 
 const routes = Router();
 
-routes.get('/category', (req, res, next) => new CategoryController(req, res, next).findAll());
+routes.post('/auth/login', (req, res, next) => new AuthController(req, res, next).login());
 
-routes.get('/product/:id', (req, res, next) => new ProductController(req, res, next).findById());
+routes.get('/category', 
+(req, res, next) => new ValidateJWT(req, res, next).validate(), 
+(req, res, next) => new CategoryController(req, res, next).findAll());
 
-routes.get('/product', (req, res, next) => new ProductController(req, res, next).findAll());
+routes.get('/product/:id',
+(req, res, next) => new ValidateJWT(req, res, next).validate(),
+(req, res, next) => new ProductController(req, res, next).findById());
 
-routes.patch('/product/:id', (req, res, next) => new ProductController(req, res, next).update());
+routes.get('/product', 
+(req, res, next) => new ValidateJWT(req, res, next).validate(),
+(req, res, next) => new ProductController(req, res, next).findAll());
 
-routes.post('/product', (req, res, next) => new ProductController(req, res, next).create());
+routes.patch('/product/:id', 
+(req, res, next) => new ValidateJWT(req, res, next).validate(),
+(req, res, next) => new ProductController(req, res, next).update());
 
-routes.delete('/product/:id', (req, res, next) => new ProductController(req, res, next).delete());
+routes.post('/product',
+(req, res, next) => new ValidateJWT(req, res, next).validate(),
+(req, res, next) => new ProductController(req, res, next).create());
+
+routes.delete('/product/:id',
+(req, res, next) => new ValidateJWT(req, res, next).validate(),
+(req, res, next) => new ProductController(req, res, next).delete());
 
 export default routes;
